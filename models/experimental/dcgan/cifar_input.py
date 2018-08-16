70db02f3bd48ce49018cf93916d6445be5442418
boscoybarra <bosco@ybarra.io>
2018-08-16 16:31:03 +0200

    arrange of CIFAR

--- models/experimental/dcgan/cifar_input.py
+++ models/experimental/dcgan/cifar_input.py
@@ -43,7 +43,8 @@ def parser(serialized_example):
   # Normalize the values of the image from the range [0, 255] to [-1.0, 1.0]
   image = tf.cast(image, tf.float32) * (2.0 / 255) - 1.0
   print("HELLO line 45",image.shape)
-  image = tf.reshape(image, [3, 64*64])
+  # image = tf.reshape(image, [3, 64*64])
+  image = tf.transpose(tf.reshape(image, [3, 64*64]))
   print("HELLO line 47",image.shape)
   # label = tf.cast(features['label'], tf.int32)
   # return image, label
