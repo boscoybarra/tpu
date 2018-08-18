a67e3d6f44977c1cdf479c3af2cf3f4905090a0c
boscoybarra <bosco@ybarra.io>
2018-08-18 17:53:02 +0200

    updating cifar

--- models/experimental/dcgan/cifar_input.py
+++ models/experimental/dcgan/cifar_input.py
@@ -103,7 +103,7 @@ class InputFunction(object):
       dataset = dataset.map(_parse_function)
       dataset = dataset.prefetch(4 * batch_size).cache().repeat()
       dataset = dataset.apply(
-        tf.data.Dataset.batch(batch_size))
+        dataset.batch(batch_size))
       dataset = dataset.prefetch(2)
       images = dataset.make_one_shot_iterator().get_next()
       print("L89",images)
