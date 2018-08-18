ae3925696122f9547b9e990eeb034e656d4ea1a2
boscoybarra <bosco@ybarra.io>
2018-08-18 18:45:38 +0200

    updating cifar

--- models/experimental/dcgan/test.py
+++ models/experimental/dcgan/test.py
@@ -1,4 +1,11 @@
 import tensorflow as tf
-with tf.gfile.GFile("gs://ptosis-test/data/img/", "r") as f:
-        content = f.readlines()
-        print(content)
+# with tf.gfile.GFile("gs://ptosis-test/data/img/", "r") as f:
+#         content = f.readlines()
+#         print(content)
+filename = "gs://ptosis-test/data/img/*.jpg"
+
+with gfile.GFile(filename, "r") as mapping_file:
+    lines = mapping_file.readlines()
+    mapping = dict([_.split("\t")[0:2] for _ in lines])
+    mapping = {k.strip(): v.strip() for k, v in mapping.items()}
+return mapping 
