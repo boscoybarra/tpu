8dc7afa55c3729ae9e35df5ba0788d7a2dbcdc39
boscoybarra <bosco@ybarra.io>
2018-08-18 17:26:16 +0200

    updating cifar

--- models/experimental/dcgan/cifar_input.py
+++ models/experimental/dcgan/cifar_input.py
@@ -26,7 +26,7 @@ import tensorflow as tf
 
 FLAGS = flags.FLAGS
 
-flags.DEFINE_string('cifar_train_data_file', '/data/img/*.jpg',
+flags.DEFINE_string('cifar_train_data_file', 'gs://ptosis-test/data/img/',
                     'Path to CIFAR10 training data.')
 
 
