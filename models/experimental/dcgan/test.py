b77c6ab9e0cd63c85cec41c48069d970ea5cbd61
boscoybarra <bosco@ybarra.io>
2018-08-18 18:48:00 +0200

    updating cifar

--- models/experimental/dcgan/test.py
+++ models/experimental/dcgan/test.py
@@ -4,8 +4,20 @@ import tensorflow as tf
 #         print(content)
 filename = "gs://ptosis-test/data/img/*.jpg"
 
-with gfile.GFile(filename, "r") as mapping_file:
+def _get_unk_mapping(filename):
+  """Reads a file that specifies a mapping from source to target tokens.
+  The file must contain lines of the form <source>\t<target>"
+
+  Args:
+    filename: path to the mapping file
+
+  Returns:
+    A dictionary that maps from source -> target tokens.
+  """
+  with gfile.GFile(filename, "r") as mapping_file:
     lines = mapping_file.readlines()
     mapping = dict([_.split("\t")[0:2] for _ in lines])
     mapping = {k.strip(): v.strip() for k, v in mapping.items()}
-    return mapping
+  return mapping
+
+print(_get_unk_mapping(filename))
