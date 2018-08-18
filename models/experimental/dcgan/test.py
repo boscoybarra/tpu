b48e4757cc24baed7abfa64c4f8bd164fbd3f58d
boscoybarra <bosco@ybarra.io>
2018-08-18 18:41:46 +0200

    updating cifar

--- models/experimental/dcgan/test.py
+++ models/experimental/dcgan/test.py
@@ -1,4 +1,4 @@
 import tensorflow as tf
-with tf.gfile.GFile("gs://ptosis-test/data/img/*.jpg", "r") as f:
+with tf.gfile.GFile("gs://ptosis-test/data/img/", "r") as f:
         content = f.readlines()
         print(content)
