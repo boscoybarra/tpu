3b214768646f0e177c4d06c7458d867127017482
boscoybarra <bosco@ybarra.io>
2018-08-18 19:00:11 +0200

    updating cifar

--- models/experimental/dcgan/test.py
+++ models/experimental/dcgan/test.py
@@ -16,8 +16,8 @@ def _get_unk_mapping(filename):
   """
   with tf.gfile.GFile(filename, "r") as mapping_file:
     lines = mapping_file.readlines()
-    # mapping = dict([_.split("\t")[0:2] for _ in lines])
-    # mapping = {k.strip(): v.strip() for k, v in mapping.items()}
+    mapping = dict([_.split("\t")[0:2] for _ in lines])
+    mapping = {k.strip(): v.strip() for k, v in mapping.items()}
   return lines
 
-print(_get_unk_mapping('gs://ptosis-test/data/img/*.jpg'))
+print(_get_unk_mapping('gs://ptosis-test/data/img/'))
