f0e63227d72abf32bd0a1ebe90902e3b1bf20151
boscoybarra <bosco@ybarra.io>
2018-08-18 16:45:11 +0200

    updating cifar

--- models/experimental/dcgan/cifar_input.py
+++ models/experimental/dcgan/cifar_input.py
@@ -68,7 +68,7 @@ def _parse_function(filename):
 def _parse_function(filename):
   image_string = tf.read_file(filename)
   image_decoded = tf.image.decode_jpeg(image_string)
-  image_resized = tf.image.resize_images(image_decoded, [28, 28])
+  image_resized = tf.image.resize_images(image_decoded, [64, 64])
   return image_resized
 
 
