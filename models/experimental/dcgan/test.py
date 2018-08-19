fafc55471255d64cec0a327492add453f7d53949
boscoybarra <bosco@ybarra.io>
2018-08-19 18:19:00 +0200

    Update Cifar for TPU

--- models/experimental/dcgan/test.py
+++ models/experimental/dcgan/test.py
@@ -2,41 +2,20 @@ import tensorflow as tf
 from PIL import Image
 import numpy as np
 
-
+urls= []
 names = tf.gfile.ListDirectory('gs://ptosis-test/data/img/')
+
+for name in names:
+    im = ('gs://ptosis-test/data/img/{}'.format(name))
+    urls.append(im)
+    print(urls)
+
+
+
 
 # im = map('gs://ptosis-test/data/img/{}'.format, name)
 
 # colors = ["red", "green", "blue", "purple"]
-for name in names:
-    
-    im = ('gs://ptosis-test/data/img/{}'.format(name))
-    print(im)
-
-    # IMport from dir instead of gs:// ??
-    with tf.gfile.GFile(im, "r") as f:
-        content = f.read()
-        content = np.array(content)
-        print(content)
-        # im = tf.gfile.GFile.read(n=-1)
-        r = content[:,:,0].flatten()
-        g = content[:,:,1].flatten()
-        b = content[:,:,2].flatten()
-        label = [1]
-
-        outfile = "./data"
-        out = np.array(list(label) + list(r) + list(g) + list(b),np.uint8)
-        out.tofile("out.bin")
-        np.save(outfile, out)
-
-
-# The CIFAR-10 binary format represents each example as a fixed-length record with the following format:
-
-# 1-byte label.
-# 1 byte per pixel for the red channel of the image.
-# 1 byte per pixel for the green channel of the image.
-# 1 byte per pixel for the blue channel of the image.
-# Assuming you have a list of image filenames called images, and a list of integers (less than 256) called labels corresponding to their labels, the following code would write a single file containing these images in CIFAR-10 format:
 
 # with open(output_filename, "wb") as f:
 #   for label, img in zip(labels, images): 
@@ -47,3 +26,25 @@ for name in names:
 #     f.write(im[:, :, 0].tostring())  # Write red channel.
 #     f.write(im[:, :, 1].tostring())  # Write green channel.
 #     f.write(im[:, :, 2].tostring())  # Write blue channel.
+
+# for name in names:
+    
+#     im = ('gs://ptosis-test/data/img/{}'.format(name))
+#     print(im)
+
+#     # IMport from dir instead of gs:// ??
+#     with tf.gfile.GFile(im, "r") as f:
+#         content = f.read()
+#         content = np.array(content)
+#         print(content)
+#         # im = tf.gfile.GFile.read(n=-1)
+#         r = content[:,:,0].flatten()
+#         g = content[:,:,1].flatten()
+#         b = content[:,:,2].flatten()
+#         label = [1]
+
+#         outfile = "./data"
+#         out = np.array(list(label) + list(r) + list(g) + list(b),np.uint8)
+#         out.tofile("out.bin")
+#         np.save(outfile, out)
+
