ddce01ee7a776e7011ba077d19147f561f51cd73
boscoybarra <bosco@ybarra.io>
2018-08-19 13:38:25 +0200

    Update Cifar for TPU

--- models/experimental/dcgan/test.py
+++ models/experimental/dcgan/test.py
@@ -9,10 +9,15 @@ print(len(name))
 
 
 # im = map('gs://ptosis-test/data/img/{}'.format, name)
-im = Image.open(tf.gfile.ListDirectory('gs://ptosis-test/data/img/{}'.format, name))
-print(im)
-im = (np.array(im))
-print("HOLA", im)
+# for n in name:
+# im = Image.open(tf.gfile.ListDirectory('gs://ptosis-test/data/img/{}'.format, name))
+# print(im)
+# im = (np.array(im))
+# print("HOLA", im)
+
+filenames = [os.path.join('gs://ptosis-test/data/img/{}'.format, name)
+                 for i in xrange(0, 669)]
+print("Hola", filename)
 
 r = im[:,:,0].flatten()
 g = im[:,:,1].flatten()
