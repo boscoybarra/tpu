49ae1821fb5f31e2f053e6f3a51911aeefdfece4
boscoybarra <bosco@ybarra.io>
2018-08-22 23:30:31 +0200

    parsing data from tfrecords to TPU infeed

--- models/experimental/dcgan/cifar_input.py
+++ models/experimental/dcgan/cifar_input.py
@@ -23,6 +23,7 @@ from absl import flags
 import numpy as np
 from PIL import Image
 import tensorflow as tf
+import resnet_preprocessing
 
 FLAGS = flags.FLAGS
 
@@ -31,42 +32,55 @@ flags.DEFINE_string('cifar_train_data_file', 'gs://ptosis-test/data/train-00000-
 flags.DEFINE_string('cifar_test_data_file', 'gs://ptosis-test/data/validation-00000-of-00001', 'Path to CIFAR10 test data.')
 
 
-def parser(serialized_example):
+def parser(self, value):
   """Parses a single tf.Example into image and label tensors."""
-  features = tf.parse_single_example(
-      serialized_example,
-      features= {
+  keys_to_features = {
           'image': tf.FixedLenFeature((), tf.string, ''),
-          'label': tf.FixedLenFeature([], tf.int64)
-      })
-  image = tf.decode_raw(features['image'], tf.uint8)
-  image.set_shape([3*64*64])
+          'label': tf.FixedLenFeature([], tf.int64),
+      }
+
+  parsed = tf.parse_single_example(value, keys_to_features)
+  image_bytes = tf.reshape(parsed['image'], shape=[])
+
+  image = self.image_preprocessing_fn(
+      image_bytes=image_bytes,
+      is_training=self.is_training,
+      image_size=self.image_size,
+      use_bfloat16=self.use_bfloat16)
+
+    # Subtract one so that labels are in [0, 1000).
+  label = tf.cast(
+      tf.reshape(parsed['label'], shape=[]), dtype=tf.int32) - 1
+
+  
+  # image.set_shape([3*64*64])
   # Normalize the values of the image from the range [0, 255] to [-1.0, 1.0]
   image = tf.cast(image, tf.float32) * (2.0 / 255) - 1.0
   image = tf.transpose(tf.reshape(image, [3, 64*64]))
-  label = tf.cast(features['label'], tf.int32)
   return image, label
 
 
 class InputFunction(object):
   """Wrapper class that is passed as callable to Estimator."""
 
-  def __init__(self, is_training, noise_dim):
+  def __init__(self, is_training, noise_dim, use_bfloat16, image_size=64, num_cores=1):
     self.is_training = is_training
     self.noise_dim = noise_dim
     self.data_file = (FLAGS.cifar_train_data_file if is_training
                       else FLAGS.cifar_test_data_file)
+    self.num_cores = num_cores
+    self.image_preprocessing_fn = resnet_preprocessing.preprocess_image
+    self.image_size = image_size
+    self.use_bfloat16 = use_bfloat16
 
   def __call__(self, params):
     batch_size = params['batch_size']
-    dataset = tf.data.TFRecordDataset(
-         self.data_file, buffer_size=4096)
-    dataset = dataset.repeat()
+    dataset = tf.data.TFRecordDataset(self.data_file)
+    dataset = dataset.map(parser, num_parallel_calls=batch_size)
+    dataset = dataset.prefetch(4 * batch_size).cache().repeat()
     dataset = dataset.apply(
-      tf.contrib.data.map_and_batch(
-          parser, batch_size=batch_size,
-         num_parallel_batches=1,
-         drop_remainder=True))
+        tf.contrib.data.batch_and_drop_remainder(batch_size))
+    dataset = dataset.prefetch(2)
     images, labels = dataset.make_one_shot_iterator().get_next()
 
     # Reshape to give inputs statically known shapes.
