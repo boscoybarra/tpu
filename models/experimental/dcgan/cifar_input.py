83e88f3ecece1276df9a6a62d82aae1061c16f0c
boscoybarra <bosco@ybarra.io>
2018-08-23 14:19:43 +0200

    adjusting hyper params

--- models/experimental/dcgan/cifar_input.py
+++ models/experimental/dcgan/cifar_input.py
@@ -32,10 +32,10 @@ flags.DEFINE_string('cifar_train_data_file', 'gs://ptosis-test/data/train-00000-
                     'Path to CIFAR10 training data.')
 flags.DEFINE_string('cifar_test_data_file', 'gs://ptosis-test/data/validation-00000-of-00001', 'Path to CIFAR10 test data.')
 flags.DEFINE_string('data_dir', 'gs://ptosis-test/data/', 'Directory where input data is stored.')
-flags.DEFINE_integer('initial_shuffle_buffer_size', 4096,'Number of elements from dataset that shuffler will sample from. ''This shuffling is done before any other operations. ''Set to 0 to disable')
+flags.DEFINE_integer('initial_shuffle_buffer_size', 1024,'Number of elements from dataset that shuffler will sample from. ''This shuffling is done before any other operations. ''Set to 0 to disable')
 flags.DEFINE_integer('followup_shuffle_buffer_size', 0,'Number of elements from dataset that shuffler will sample from. ''This shuffling is done after prefetching is done. ''Set to 0 to disable')
 flags.DEFINE_integer('num_files_infeed', 1, 'Number of training files to read in parallel.')
-flags.DEFINE_integer('prefetch_dataset_buffer_size', 1*4096*4096,'Number of bytes in read buffer. 0 means no buffering.')
+flags.DEFINE_integer('prefetch_dataset_buffer_size', 1*1024*1024,'Number of bytes in read buffer. 0 means no buffering.')
 flags.DEFINE_integer('num_parallel_calls', default=64, help=('Number of parallel threads in CPU for the input pipeline'))
 
 
@@ -108,7 +108,7 @@ class InputFunction(object):
   def parser(self, value):
     """Parses a single tf.Example into image and label tensors."""
     keys_to_features = {
-            'image': tf.FixedLenFeature((), tf.string, ''),
+            'image': tf.FixedLenFeature((), tf.string),
             'label': tf.FixedLenFeature([], tf.int64),
         }
     print("YOOO",keys_to_features)
