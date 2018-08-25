52c8fa7a2d744838234a17597d7854462c72882e
boscoybarra <bosco@ybarra.io>
2018-08-25 17:32:40 +0200

    creating the proper tfrecords to make my model start training

--- models/experimental/dcgan/cifar_input.py
+++ models/experimental/dcgan/cifar_input.py
@@ -111,7 +111,6 @@ class InputFunction(object):
 
       # Perform additional preprocessing on the parsed data.
       image = tf.image.decode_jpeg(parsed["real_images"])
-      image.set_shape([3*64*64])
       # Normalize the values of the image from the range [0, 255] to [-1.0, 1.0]
       image = tf.cast(image, tf.float32) * (2.0 / 255) - 1.0
       image = tf.transpose(tf.reshape(image, [3, 64*64]))
